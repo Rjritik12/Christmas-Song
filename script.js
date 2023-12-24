1509c19adf79e44f29ffd4aaaec271186114ae46
ritik jaiswal <110723218+Rjritik12@users.noreply.github.com>
2023-12-24 22:44:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+alert("Tap anywhere to play music🎶..")
+alert("🎅May the spirit of christmas infuse ur life & that of ur family members with hope, positivity & joy.🎅")
+alert("UPVOTE if u like...🙂")
+window.addEventListener("touchend",play);
+function play(){document.getElementById("song").play();} 
